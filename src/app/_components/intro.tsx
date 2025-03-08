8740f895cfe8237b409227882a20c87231f2defb
kazukiaidjudge@gmail.com <kazukiaidjudge@gmail.com>
2025-03-08 20:17:30 +0900

    Fix Github Actions経由でのデプロイの確認

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,10 +4,10 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Tech × Care Family Lab.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
+        朗らかエンジニアと穏やか助産師のゆるゆる挑戦記{" "}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-blue-600 duration-200 transition-colors"
